aca9316d000b99a300908d395949adf5596ce8c4
samiricium <sameerpantz@gmail.com>
2023-12-14 14:11:02 +0545

    v2

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv("/home/samir/Desktop/rudraAnalytics/sub_projects/churn/data.csv")
-
-for x in df.columns:
-    print(df[x].value_counts())
-    print(f"""
-        FOR {x}
-        ####################################################
-    """)
